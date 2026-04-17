eb88f25529a18a4cb69bf4b5123b6f03af084f6a
uemrey0 <ufukemreyuceturk@hotmail.com>
2026-04-18 01:25:33 +0200

    change api

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,3 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? ''
-
 export function apiFetch(path: string, init?: RequestInit): Promise<Response> {
-  return fetch(`${BASE_URL}${path}`, init)
+  return fetch(path, init)
 }
